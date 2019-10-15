ace9b8cb46367e767b418348471e8d8634e3682b
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-10-15 13:37:16 -0600

    Extension Function(notification)
    add notification builder class

--- M2 Assignment/app/src/main/java/com/example/extensionfunctionsusinglambda/MainActivity.kt
+++ M2 Assignment/app/src/main/java/com/example/extensionfunctionsusinglambda/MainActivity.kt
@@ -5,6 +5,12 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    companion object{
+        const val NOTIFICATION_ID = 1
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
