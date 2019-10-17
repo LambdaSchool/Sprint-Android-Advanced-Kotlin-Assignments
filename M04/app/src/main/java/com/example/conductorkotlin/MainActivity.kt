4d843a442dae1596965cecc95cddff5530d7b52d
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-10-17 14:46:27 -0600

    MainActivity
    attach router to the activity

--- M04/app/src/main/java/com/example/conductorkotlin/MainActivity.kt
+++ M04/app/src/main/java/com/example/conductorkotlin/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.conductorkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bluelinelabs.conductor.Conductor
+import com.bluelinelabs.conductor.Router
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    // todo 1 attach router to the activity
+    private lateinit var router: Router
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        router = Conductor.attachRouter(this, controller_container, savedInstanceState)
     }
 }
